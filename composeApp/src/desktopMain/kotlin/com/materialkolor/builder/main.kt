4505510b314cdee1258a7390ab4368538f3a9265
Jordon de Hoog <jordon.dehoog@gmail.com>
2024-09-22 15:03:58 -0400

    add desktop icon

--- composeApp/src/desktopMain/kotlin/com/materialkolor/builder/main.kt
+++ composeApp/src/desktopMain/kotlin/com/materialkolor/builder/main.kt
@@ -3,11 +3,15 @@ package com.materialkolor.builder
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import com.materialkolor.builder.ui.App
+import materialkolorbuilder.composeapp.generated.resources.Res
+import materialkolorbuilder.composeapp.generated.resources.icon
+import org.jetbrains.compose.resources.painterResource
 
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = "MaterialKolorBuilder",
+        icon = painterResource(Res.drawable.icon),
     ) {
         App()
     }
