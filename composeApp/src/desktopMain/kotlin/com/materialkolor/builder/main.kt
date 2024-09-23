33825b2d66f04ab510d7971efc02c72ffe1ea4ea
Jordon de Hoog <jordon.dehoog@gmail.com>
2024-09-23 15:59:52 -0400

    update upstream

--- composeApp/src/desktopMain/kotlin/com/materialkolor/builder/main.kt
+++ composeApp/src/desktopMain/kotlin/com/materialkolor/builder/main.kt
@@ -10,7 +10,7 @@ import org.jetbrains.compose.resources.painterResource
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
-        title = "MaterialKolorBuilder",
+        title = "MaterialKolor Builder",
         icon = painterResource(Res.drawable.icon),
     ) {
         App()
