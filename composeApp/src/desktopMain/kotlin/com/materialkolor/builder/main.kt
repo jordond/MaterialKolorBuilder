5ed8e6155bedf76ca0ce497c5ba1747d9dd0aa91
Jordon de Hoog <jordon.dehoog@gmail.com>
2024-09-23 16:03:22 -0400

    Update main.kt

--- composeApp/src/desktopMain/kotlin/com/materialkolor/builder/main.kt
+++ composeApp/src/desktopMain/kotlin/com/materialkolor/builder/main.kt
@@ -1,7 +1,9 @@
 package com.materialkolor.builder
 
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPlacement
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 import com.materialkolor.builder.ui.App
 import materialkolorbuilder.composeapp.generated.resources.Res
 import materialkolorbuilder.composeapp.generated.resources.icon
@@ -12,6 +14,7 @@ fun main() = application {
         onCloseRequest = ::exitApplication,
         title = "MaterialKolor Builder",
         icon = painterResource(Res.drawable.icon),
+        state = rememberWindowState(placement = WindowPlacement.Maximized),
     ) {
         App()
     }
