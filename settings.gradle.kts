8d43ea1d9d326bef21fc87f764ea42f883adc4c4
Jordon de Hoog <jordon.dehoog@gmail.com>
2024-09-25 20:09:46 -0400

    add develocity

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -29,6 +29,21 @@ dependencyResolutionManagement {
     }
 }
 
+plugins {
+    id("com.gradle.develocity") version "3.18.1"
+}
+
+develocity {
+    buildScan {
+        termsOfUseUrl.set("https://gradle.com/help/legal-terms-of-use")
+        termsOfUseAgree.set("yes")
+
+        publishing.onlyIf { context ->
+            context.buildResult.failures.isNotEmpty() && !System.getenv("CI").isNullOrEmpty()
+        }
+    }
+}
+
 include(":composeApp")
 includeBuild("library") {
     dependencySubstitution {
